90e51cdab8cd8d454f931342437e3d0502e3368d
asusikov <susikov.alexander@gmail.com>
2022-07-11 13:04:28 +0300

    Added live grep for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,20 @@
-require('telescope').setup{
-}
+local actions = require("telescope.actions")
+
+require('telescope').setup({
+  defaults = {
+      mappings = {
+          i = {
+              ["<esc>"] = actions.close,
+          },
+      },
+  },
+})
+
+local km = vim.keymap
+
+km.set("n", "fg", function()
+  require("telescope.builtin").live_grep()
+end)
 -- nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
 -- nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
 -- nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
