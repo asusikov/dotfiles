abf72a0cf7f74169e15555d8f88fe0448627b5e1
asusikov <susikov.alexander@gmail.com>
2023-11-27 23:24:41 +0300

    Updated luasnip settings

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -2,9 +2,9 @@ require('luasnip.loaders.from_vscode').lazy_load()
 
 local ls = require('luasnip')
 
-vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-H>", function() ls.jump(-1) end, {silent = true})
+vim.keymap.set({"i"}, "<C-u>", function() ls.expand() end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-j>", function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-k>", function() ls.jump(-1) end, {silent = true})
 
 vim.keymap.set({"i", "s"}, "<C-E>", function()
 	if ls.choice_active() then
