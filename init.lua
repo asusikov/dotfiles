b5ab32fba51dda96efaa2618462712f7ee2e105e
asusikov <susikov.alexander@gmail.com>
2023-11-29 00:53:38 +0300

    Added border for lazy window

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
 require("core")
 
-require("lazy").setup({ import = "plugins" }, {})
+require("lazy").setup({ import = "plugins" }, {
+  install = {
+    colorscheme = { "nord" },
+  },
+  ui = {
+    border = "single",
+  },
+})
