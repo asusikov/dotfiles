18bfc2cc3a5603648b140f7791a8a0a468d77933
asusikov <susikov.alexander@gmail.com>
2023-11-29 20:58:42 +0300

    Added linting and formatting

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,26 @@
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		conform = require("conform")
+
+		format_settings = {
+			async = false,
+			lsp_fallback = true,
+			timeout_ms = 1000,
+		}
+
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				ruby = { "rubocop" },
+				javascript = { { "prettierd", "prettier" } },
+				go = { "goimports", "gofumpt" },
+			},
+			format_on_save = format_settings,
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>fb", function()
+			conform.format(format_settings)
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
+}
