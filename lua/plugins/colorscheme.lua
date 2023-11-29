24da411ff071fcd38c3edeaef6a2768afa4a27ae
asusikov <susikov.alexander@gmail.com>
2023-11-29 20:58:29 +0300

    Turn off bold font

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,10 @@
 return {
-    -- 'shaunsingh/nord.nvim',
-  'arcticicestudio/nord-vim',
-    priority = 1000, 
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme nord]])
-    end,
+	-- 'shaunsingh/nord.nvim',
+	"arcticicestudio/nord-vim",
+	priority = 1000,
+	config = function()
+		vim.g.nord_bold = 0
+
+		vim.cmd([[colorscheme nord]])
+	end,
 }
