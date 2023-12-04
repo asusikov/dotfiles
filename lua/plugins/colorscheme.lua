84b2b67dfc7d662bf79c94cb013dd7455c30f7ed
asusikov <susikov.alexander@gmail.com>
2023-12-04 13:32:31 +0300

    Added own colours

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,5 +6,13 @@ return {
 		vim.g.nord_bold = 0
 
 		vim.cmd([[colorscheme nord]])
+
+		vim.cmd([[hi Statement gui=NONE]])
+		vim.cmd([[hi RubyConstant guifg=#ECEFF4]])
+		vim.cmd([[hi RubyConstant guifg=#ECEFF4]])
+		vim.cmd([[hi RubyConstant guifg=#ECEFF4]])
+		vim.cmd([[hi RubyConstant guifg=#ECEFF4]])
+		vim.cmd([[hi String guifg=#88C0D0]])
+		vim.cmd([[hi Number guifg=#88C0D0]])
 	end,
 }
