e7149c74b601314ddd1a776a67dde4ddce240461
asusikov <susikov.alexander@gmail.com>
2023-12-19 21:47:37 +0300

    Updated key for closing of terminal

--- lua/plugins/vimux.lua
+++ lua/plugins/vimux.lua
@@ -5,7 +5,7 @@ return {
 		vim.keymap.set("n", "to", function()
 			vim.cmd("VimuxOpenRunner")
 		end)
-		vim.keymap.set("n", "tq", function()
+		vim.keymap.set("n", "<leader>tq", function()
 			vim.cmd("VimuxCloseRunner")
 		end)
 		vim.keymap.set("n", "tb", function()
