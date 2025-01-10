633778232ba27c741433cf24fc1cdebef4d8fabc
asusikov <susikov.alexander@gmail.com>
2025-01-10 10:32:59 +0300

    Turn off codeium

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,20 +1,20 @@
 return {
-  'Exafunction/codeium.vim',
-  config = function ()
-    vim.keymap.set("i", "<C-u>", function()
-      return vim.fn["codeium#Accept"]()
-    end, { expr = true })
-    vim.keymap.set("i", "<C-j>", function()
-      return vim.fn["codeium#CycleCompletions"](1)
-    end, { expr = true })
-    vim.keymap.set("i", "<C-k>", function()
-      return vim.fn["codeium#CycleCompletions"](-1)
-    end, { expr = true })
-    vim.keymap.set("i", "<C-c>", function()
-      return vim.fn["codeium#Clear"]()
-    end, { expr = true })
-    vim.keymap.set("i", "<C-e>", function()
-      return vim.fn["codeium#Complete"]()
-    end, { expr = true })
-  end
+	-- 'Exafunction/codeium.vim',
+	-- config = function ()
+	--   vim.keymap.set("i", "<C-u>", function()
+	--     return vim.fn["codeium#Accept"]()
+	--   end, { expr = true })
+	--   vim.keymap.set("i", "<C-j>", function()
+	--     return vim.fn["codeium#CycleCompletions"](1)
+	--   end, { expr = true })
+	--   vim.keymap.set("i", "<C-k>", function()
+	--     return vim.fn["codeium#CycleCompletions"](-1)
+	--   end, { expr = true })
+	--   vim.keymap.set("i", "<C-c>", function()
+	--     return vim.fn["codeium#Clear"]()
+	--   end, { expr = true })
+	--   vim.keymap.set("i", "<C-e>", function()
+	--     return vim.fn["codeium#Complete"]()
+	--   end, { expr = true })
+	-- end
 }
