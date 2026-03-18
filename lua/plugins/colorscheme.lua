136a61a03dbd8a33e7821a9abd6264998589fef9
Alexander Susikov <alexandersusikov@Alexanders-MacBook-Pro.local>
2026-03-18 16:52:58 +0300

    Updated vim color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,18 @@
 return {
 	-- 'shaunsingh/nord.nvim',
 	-- "catppuccin/nvim",
-  "EdenEast/nightfox.nvim",
+-- c  "EdenEast/nightfox.nvim",
+'vpoltora/cursor-light.nvim',
 	priority = 1000,
 	config = function()
 		-- vim.g.nord_bold = 0
 		--
-    require('nightfox').setup({
-
-    })
-
-		vim.cmd([[colorscheme dawnfox]])
+    -- require('nightfox').setup({
+    --
+    -- })
+require('cursor-light').setup()
+    vim.cmd.colorscheme('cursor-light')
+		-- vim.cmd([[colorscheme dawnfox]])
 		--
 		-- vim.cmd([[hi Statement gui=NONE]])
 		-- vim.cmd([[hi RubyConstant guifg=#ECEFF4]])
